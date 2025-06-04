2757564a17b6647678a621fc77506ac7f62516e8
Renan Silva <renansilvaytB@gmail.com>
2025-06-03 22:00:09 -0300

    feat: navegacao slide

---
+++ scripts/slide/script.js
@@ -0,0 +1,8 @@
+import SlideNav from './slide_nav.js'
+
+const slide = document.querySelector('.slide')
+const wrapper = document.querySelector('.slide-wrapper')
+
+const oSlide = new SlideNav(slide, wrapper)
+oSlide.init()
+oSlide.addArrow('.prev', '.next')
